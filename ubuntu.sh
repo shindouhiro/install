8afbde6d5a43598b3c43b261f95375a62e4d85fb
hikaru <274173368@qq.com>
2021-01-25 14:17:29 +0800

    qqX

---
+++ ubuntu.sh
@@ -0,0 +1,28 @@
+echo "Install Curl"
+sudo apt-get install curl -y
+echo "-------------------------Install Curl Successed----------------------------"
+echo "Install Wget"
+sudo apt-get install wget -y
+echo "-------------------------Install Wget Successed----------------------------"
+
+
+echo "Install Git"
+sudo apt-get install git -y
+echo "-------------------------Install Git Successed----------------------------"
+git config --global user.name "hikaru"
+git config --global user.email "274173368@qq.com"
+git config --global alias.co checkout 
+git config --global alias.ci commit
+git config --global alias.br branch
+echo "-------------------------Set Git Config Successed----------------------------"
+
+echo "Instll Xclip"
+sudo apt-get install xclip -y 
+echo "-------------------------Install Xclip Successed----------------------------"
+
+echo "Install Node"
+sudo apt-get install node -y
+echo "-------------------------Install Node Successed----------------------------"
+echo "Install Nrm"
+# sudo npm i -g nrm
+echo "-------------------------Install Nrm Successed----------------------------"
