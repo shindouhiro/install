294cfb31671b4d371bdcd586fe4c6d56f50f73ce
hikaru <274173368@qq.com>
2021-01-25 14:22:49 +0800

    修改git配置

--- ubuntu.sh
+++ ubuntu.sh
@@ -14,6 +14,7 @@ git config --global user.email "274173368@qq.com"
 git config --global alias.co checkout 
 git config --global alias.ci commit
 git config --global alias.br branch
+git config --global alias.st status
 echo "-------------------------Set Git Config Successed----------------------------"
 
 echo "Instll Xclip"
